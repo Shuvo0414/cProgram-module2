c5f4dd79a230306effbc4c2e791579b796e865c8
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2026-03-28 15:20:02 +0600

    Relational operqtors

---
+++ relationalOp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+
+/*
+    Relational operators compare two values and return true or false.
+
+    >   greater than.
+    >=  greater than or equal to.
+    <   less than.
+    <=  less than or equal to.
+    ==  equal to.
+    !=  not equal to.
+*/
+
+
+
+    return 0;
+}
