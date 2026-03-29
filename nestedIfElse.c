12d572f5be868999f1f502b886ab8d9b67725495
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2026-03-29 13:42:49 +0600

    Practice nested if-else using travel cost example

---
+++ nestedIfElse.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    // Nested IF , Else.
+
+    int tk; 
+    scanf("%d", &tk);
+    if (tk >= 5000)
+    {
+    printf("I want to go to Cox's Bazar\n");
+
+    // nested condition
+    if (tk >= 10000)
+    {
+        printf("I also want to visit Saint Martin from Cox's Bazar\n");
+    }
+    else
+    {
+        printf("I will return from Cox's Bazar. Not enough money to visit Saint Martin\n");
+    }
+    }
+    else
+    {
+    printf("I am not going anywhere, I do not have enough money\n");
+    }
+    
+
+/*
+    Nested if-else means an if statement inside another if statement.
+
+    First condition:
+    If tk >= 5000 → go to Cox's Bazar
+
+    Second condition (inside first if):
+    If tk >= 10000 → also visit Saint Martin
+    Else → return from Cox's Bazar because not enough money
+
+    Else:
+    If tk < 5000 → not going anywhere
+*/
+
+
+
+
+    return 0;
+}
